d47adc9704c461cee48969bf7094dddb9f696dcf
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-01-08 11:12:55 +0530

    Update PostgreSQL settings and enhance PL/pgSQL documentation; rename connection, add string manipulation examples, and introduce variable usage and error handling in PL/pgSQL.

--- database/relational/postgres/12_postgres-function/string.sql
+++ database/relational/postgres/12_postgres-function/string.sql
@@ -10,6 +10,10 @@ SELECT
     INITCAP('hi there');
 
 
+-- "SPLIT_PART('a-b-c-d', '-', 3) -> 'c'": It splits the string by given delimiter and returns the part based on given index.
+SELECT
+    SPLIT_PART('a-b-c-d', '-', 3); -- It's similar to .split() in other programming languages.
+
 -- "LENGTH('hi there')": Returns the length of the given sentence
 -- "LOWER('HI THERE')": Convert it into lower case
 -- "UPPER('hi there')": Convert it into upper case
