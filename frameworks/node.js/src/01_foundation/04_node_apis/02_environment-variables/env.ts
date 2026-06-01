2b385fd2cd84aba2852173d9227991b8d1e7538a
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-01 12:37:11 +0530

    Add dotenv dependency and implement environment variable loading; create path utility functions

---
+++ frameworks/node.js/src/01_foundation/04_node_apis/02_environment-variables/env.ts
@@ -0,0 +1,15 @@
+try {
+  process.loadEnvFile("./.env"); // Came in "node v24" -> Used to load the .env from a particular file.
+
+  // Let's print all .envs, every "environment variable" can be accessed through process.env can gave "environment variable" as object.
+  // 👉 It gave all environment variables available into the system and using "dotenv", "loadEnvFile" it combine these with system environment variables.
+  console.log("All environment variables are: ", process.env);
+
+  // Let's access some particular one.
+  console.log("PORT NO is: ", process.env["PORT"]);
+
+  // 🟡 Like we are loading custom "environment variables" using "loadEnvFile", there are few CLI options also.
+  // "--env-file=file" -> We can also add multiple files using it.
+} catch {
+  console.error("No .env file exists");
+}
