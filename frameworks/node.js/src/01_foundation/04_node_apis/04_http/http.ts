b2ae47c31b6dc28456e96e3eaf58526da247fbff
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-03 13:09:12 +0530

    Update dependencies and add HTTP server implementation
    - Bump express version to 5.2.1
    - Add @types/express to devDependencies
    - Introduce a new HTTP server in http.ts with basic request handling
    - Remove obsolete .gitkeep file from HTTP directory

---
+++ frameworks/node.js/src/01_foundation/04_node_apis/04_http/http.ts
@@ -0,0 +1,76 @@
+import http from "node:http";
+
+const sleep = (time = 1000) =>
+  new Promise((resolve) => setTimeout(() => resolve(""), 1000));
+
+/*
+ 🟡 In node.js to create a simple "http" server we use --> "http.createServer" function, it accept one callback function that gives "Request" and "Response"
+    🔷 "Request" --> It's a ReadableStream, So that we can use like a "node.stream" like we can use "pipes" etc
+    🔷 "Response" --> It's a WritableStream, So that we can directly pass data through pipes.
+ 
+ 
+ 📥 Important methods and variables (REQUEST)
+    1. Properties
+        1. req.url
+        2. req.method
+        3. req.headers
+   
+    2. Stream Events
+        1. req.on('data', cb)
+        2. req.on('end', cb)
+        2. req.on('error', cb)
+        2. req.pip(dest)
+ 
+ 📤 Important methods and variables (RESPONSE)
+    1. Properties
+        1. res.statusCode = 200
+        2. res.writeableEnded
+        3. res.finished
+        
+    2. Header Methods
+        1. res.setHeader(name, value);
+        2. res.getHeader(name, value);
+        3. res.removeHeader(name)
+        4. res.writeHead(status, {})
+ 
+    3. Body methods
+        1. res.write(chuck)
+        2. res.end(data?)
+*/
+const server = http.createServer((req, res) => {
+  const { url, headers, method, pipe } = req;
+
+  if (url && url === "/test") {
+    let body: Buffer[] = [];
+
+    req
+      .on("data", (chuck) => {
+        body.push(chuck);
+      })
+      .on("end", async () => {
+        const parsedBody = Buffer.concat(body).toString();
+
+        res.write("Just ready\n");
+        await sleep(500);
+        res.write("1\n");
+        await sleep(1000);
+        res.write("2\n");
+        await sleep(1000);
+        res.write("3\n");
+        await sleep(1000);
+        res.write("Here we go\n");
+        await sleep(200);
+        res.end(parsedBody);
+      });
+  } else if (url && url === "/get") {
+    res.setHeader("body", '{"hello":"wow"}');
+    res.end('{"hello":"wow"}');
+  } else {
+    res.statusCode = 404;
+    req.pipe(res);
+  }
+});
+
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
